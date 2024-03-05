4a7b7a136a2a278e3937d0d789a9dcfd304d5279
AlpinDale <alpindale@gmail.com>
2024-03-05 19:38:29 +0000

    update requirements and include optimum

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from setuptools import setup
 from torch.utils import cpp_extension
 
 setup(
-    name='marlin',
+    name='marlin-gptq',
     version='0.1.1',
     author='Elias Frantar',
     author_email='elias.frantar@ist.ac.at',
     description='Highly optimized FP16xINT4 CUDA matmul kernel.',
-    install_requires=['numpy', 'torch'],
+    install_requires=['numpy', 'torch==2.2.0', 'optimum'],
+    url="https://github.com/AlpinDale/marlin",
     packages=['marlin'],
     ext_modules=[cpp_extension.CUDAExtension(
         'marlin_cuda', ['marlin/marlin_cuda.cpp', 'marlin/marlin_cuda_kernel.cu']
